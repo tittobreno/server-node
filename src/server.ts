80ff07d39c3bbd4e89dfd7a29fd268f8dbe1697a
unknown <brenomoraes97@yahoo.com>
2023-01-24 19:17:04 -0300

    fix: resolve

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+import express, { Request } from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", () => {
+  const user = {
+    name: "Breno",
+    age: 25,
+  };
+
+  return user;
+});
+
+app.listen(
+  {
+    port: 3333,
+  },
+  () => {
+    console.log("Server is running!");
+  }
+);
